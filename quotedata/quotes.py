7d785f3c7b8bc0e149d8f0d79b851c7ff7d8a762
vardaansinha <vardsin28@gmail.com>
2023-05-23 19:07:49 -0700

    blog backend complete

---
+++ quotedata/quotes.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_json('quotes.json')
+
+# DIFFERENT WAYS OF SORTING
+
+# OPTION 1: SORTING BY QUOTE
+print(df.sort_values('Quote'))
+
+# OPTION 2: SORTING BY NAME
+print(df.sort_values('Name'))
+
+# OPTION 3: SORTING BY DATE
+
+print(df.sort_values(by=['Year']))
+
+
+
