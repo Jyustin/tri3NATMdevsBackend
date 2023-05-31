e663806d7dddd8a079fe7e9190067b3886419333
Shaurya Goel <shauryaggamer@gmail.com>
2023-05-30 19:55:58 -0700

    asdf

--- api/pleague_players_api.py
+++ api/pleague_players_api.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request, jsonify, make_response
 from flask_restful import Api, Resource
-from model.nfl_players_model import NFLPlayer
+from models.premierleagueplayer_model import PremierLeaguePlayer
 
-nflplayers_api = Blueprint('nflplayers_api', __name__, url_prefix='/api/nflplayer')
+premierleagueplayers_api = Blueprint('premierleagueplayers_api', __name__, url_prefix='/api/premierleagueplayer')
 
-@nflplayers_api.after_request
+@premierleagueplayers_api.after_request
 def after_request(response):
     header = response.headers
     header['Access-Control-Allow-Origin'] = '*'
@@ -12,74 +12,66 @@ def after_request(response):
     header['Access-Control-Allow-Methods'] = "*"
     return response
 
-api = Api(nflplayers_api)
+api = Api(premierleagueplayers_api)
 
-class NFLPlayerAPI:
+class PremierLeaguePlayerAPI:
     class _Create(Resource):
         def post(self):
             body = request.get_json()
-            player_name = body.get('player_name')
+            name = body.get('name')
             team = body.get('team')
             position = body.get('position')
-            college = body.get('college')
             jersey_number = body.get('jersey_number')
-            height = body.get('height')
-            weight = body.get('weight')
-            # Create NFLPlayer object
-            player = NFLPlayer(
-                player_name=player_name,
+            age = body.get('age')
+            # Create PremierLeaguePlayer object
+            player = PremierLeaguePlayer(
+                name=name,
                 team=team,
                 position=position,
-                college=college,
                 jersey_number=jersey_number,
-                height=height,
-                weight=weight
+                age=age
             )
-            # Create NFL player in the database
+            # Create Premier League player in the database
             created_player = player.create()
             if created_player:
                 return jsonify(created_player.read())
-            return {'message': 'NFL player not created'}, 210
+            return {'message': 'Premier League player not created'}, 210
 
     class _Update(Resource):
         def put(self):
             body = request.get_json()
             player_id = body.get('id')
             # Validate if player exists by ID
-            player = NFLPlayer.get_player_by_id(player_id)
+            player = PremierLeaguePlayer.get_player_by_id(player_id)
             if player is None:
-                return {'message': f'NFL player with ID: ({player_id}) not found.'}, 210
-            player_name = body.get('player_name')
+                return {'message': f'Premier League player with ID: ({player_id}) not found.'}, 210
+            name = body.get('name')
             team = body.get('team')
             position = body.get('position')
-            college = body.get('college')
             jersey_number = body.get('jersey_number')
-            height = body.get('height')
-            weight = body.get('weight')
-            # Update NFLPlayer object
-            player.player_name = player_name
+            age = body.get('age')
+            # Update PremierLeaguePlayer object
+            player.name = name
             player.team = team
             player.position = position
-            player.college = college
             player.jersey_number = jersey_number
-            player.height = height
-            player.weight = weight
-            # Update NFL player in the database
+            player.age = age
+            # Update Premier League player in the database
             updated_player = player.update()
             if updated_player:
                 return jsonify(updated_player.read())
-            return {'message': f'NFL player not updated'}, 210
+            return {'message': f'Premier League player not updated'}, 210
 
     class _Read(Resource):
         def get(self):
             player_id = request.args.get("id")
             if player_id:
-                player = NFLPlayer.get_player_by_id(player_id)
+                player = PremierLeaguePlayer.get_player_by_id(player_id)
                 if player is None:
-                    return {'message': f'NFL player with ID: ({player_id}) not found.'}, 210
+                    return {'message': f'Premier League player with ID: ({player_id}) not found.'}, 210
                 return jsonify(player.read())
             else:
-                players = NFLPlayer.query.all()
+                players = PremierLeaguePlayer.query.all()
                 json_ready = [player.read() for player in players]
                 response = make_response(jsonify(json_ready), 200)
                 return response
@@ -88,18 +80,18 @@ class NFLPlayerAPI:
         def delete(self):
             player_id = request.args.get("id")
             if player_id == "":
-                return {'message': f'NFL player ID not provided.'}, 210
+                return {'message': f'Premier League player ID not provided.'}, 210
             else:
-                player = NFLPlayer.get_player_by_id(player_id)
+                player = PremierLeaguePlayer.get_player_by_id(player_id)
                 if player is None:
-                    return {'message': f'NFL player with ID: ({player_id}) not found.'}, 210
-                player_name = player.player_name
-                NFLPlayer.delete(player)
-                return {'message': f'NFL player \'{player_name}\' ({player_id}) deleted.'}, 200
+                    return {'message': f'Premier League player with ID: ({player_id}) not found.'}, 210
+                name = player.name
+                PremierLeaguePlayer.delete(player)
+                return {'message': f'Premier League player \'{name}\' ({player_id}) deleted.
 
-    api.add_resource(_Create, '/create')
-    api.add_resource(_Delete, '/delete')
-    api.add_resource(_Update, '/update')
-    api.add_resource(_Read, '/')
 
-### Complete API
+
+api.add_resource(_Create, '/create')
+api.add_resource(_Delete, '/delete')
+api.add_resource(_Update, '/update')
+api.add_resource(_Read, '/')
