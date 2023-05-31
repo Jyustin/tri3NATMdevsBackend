5858c3b486c1001d4ff9fcd16beb6a912dc2433a
Shaurya Goel <shauryaggamer@gmail.com>
2023-05-30 19:42:21 -0700

    asdf

--- model/pleague_players_model.py
+++ model/pleague_players_model.py
@@ -9,11 +9,10 @@ from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+"""Premier League Player class"""
 
-"""Defining Class and Variables"""  
-
-class NFLPlayer(db.Model):
-    __tablename__ = 'NFLPlayer'
+class PremierLeaguePlayer(db.Model):
+    __tablename__ = 'PremierLeaguePlayer'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=False, nullable=False)
@@ -23,21 +22,17 @@ class NFLPlayer(db.Model):
     age = db.Column(db.Integer, unique=False, nullable=False)
     height = db.Column(db.String(20), unique=False, nullable=False)
     weight = db.Column(db.Float, unique=False, nullable=False)
-    college = db.Column(db.String(255), unique=False, nullable=False)
-    experience = db.Column(db.Integer, unique=False, nullable=False)
-    touchdowns = db.Column(db.Integer, unique=False, nullable=False)
-    receptions = db.Column(db.Integer, unique=False, nullable=False)
-    passing_yards = db.Column(db.Integer, unique=False, nullable=False)
-    rushing_yards = db.Column(db.Integer, unique=False, nullable=False)
+    goals = db.Column(db.Integer, unique=False, nullable=False)
+    assists = db.Column(db.Integer, unique=False, nullable=False)
+    yellow_cards = db.Column(db.Integer, unique=False, nullable=False)
+    red_cards = db.Column(db.Integer, unique=False, nullable=False)
+    passes_completed = db.Column(db.Integer, unique=False, nullable=False)
     tackles = db.Column(db.Integer, unique=False, nullable=False)
-    sacks = db.Column(db.Float, unique=False, nullable=False)
-    interceptions = db.Column(db.Integer, unique=False, nullable=False)
+    clean_sheets = db.Column(db.Integer, unique=False, nullable=False)
 
+    """Constructor"""
 
-
-    """Constructor"""  
-
-    def __init__(self, name, team, position, jersey_number, age, height, weight, college, experience, touchdowns, receptions, passing_yards, rushing_yards, tackles, sacks, interceptions):
+    def __init__(self, name, team, position, jersey_number, age, height, weight, goals, assists, yellow_cards, red_cards, passes_completed, tackles, clean_sheets):
         self.name = name
         self.team = team
         self.position = position
@@ -45,20 +40,17 @@ class NFLPlayer(db.Model):
         self.age = age
         self.height = height
         self.weight = weight
-        self.college = college
-        self.experience = experience
-        self.touchdowns = touchdowns
-        self.receptions = receptions
-        self.passing_yards = passing_yards
-        self.rushing_yards = rushing_yards
+        self.goals = goals
+        self.assists = assists
+        self.yellow_cards = yellow_cards
+        self.red_cards = red_cards
+        self.passes_completed = passes_completed
         self.tackles = tackles
-        self.sacks = sacks
-        self.interceptions = interceptions
+        self.clean_sheets = clean_sheets
 
 
 
- 
-    """Setter and Getter Methods for all Variables"""  
+    """Setter and Getter Methods for all Variables"""
 
     @property
     def name(self):
@@ -116,54 +108,46 @@ class NFLPlayer(db.Model):
     def weight(self, weight):
         self._weight = weight
 
-    @property
-    def college(self):
-        return self._college
 
-    @college.setter
-    def college(self, college):
-        self._college = college
+     @property
+    def goals(self):
+        return self._goals
 
-    @property
-    def experience(self):
-        return self._experience
-
-    @experience.setter
-    def experience(self, experience):
-        self._experience = experience
+    @goals.setter
+    def goals(self, goals):
+        self._goals = goals
 
     @property
-    def touchdowns(self):
-        return self._touchdowns
+    def assists(self):
+        return self._assists
 
-    @touchdowns.setter
-    def touchdowns(self, touchdowns):
-        self._touchdowns = touchdowns
-
-    @property
-    def receptions(self):
-        return self._receptions
-
-    @receptions.setter
-    def receptions(self, receptions):
-        self._receptions = receptions
+    @assists.setter
+    def assists(self, assists):
+        self._assists = assists
 
     @property
-    def passing_yards(self):
-        return self._passing_yards
+    def yellow_cards(self):
+        return self._yellow_cards
 
-    @passing_yards.setter
-    def passing_yards(self, passing_yards):
-        self._passing_yards = passing_yards
+    @yellow_cards.setter
+    def yellow_cards(self, yellow_cards):
+        self._yellow_cards = yellow_cards
 
     @property
-    def rushing_yards(self):
-        return self._rushing_yards
+    def red_cards(self):
+        return self._red_cards
 
-    
-    @rushing_yards.setter
-    def rushing_yards(self, rushing_yards):
-        self._rushing_yards = rushing_yards
+    @red_cards.setter
+    def red_cards(self, red_cards):
+        self._red_cards = red_cards
+
+    @property
+    def passes_completed(self):
+        return self._passes_completed
+
+    @passes_completed.setter
+    def passes_completed(self, passes_completed):
+        self._passes_completed = passes_completed
 
     @property
     def tackles(self):
@@ -174,48 +158,33 @@ class NFLPlayer(db.Model):
         self._tackles = tackles
 
     @property
-    def sacks(self):
-        return self._sacks
+    def clean_sheets(self):
+        return self._clean_sheets
 
-    @sacks.setter
-    def sacks(self, sacks):
-        self._sacks = sacks
-
-    @property
-    def interceptions(self):
-        return self._interceptions
-
-    @interceptions.setter
-    def interceptions(self, interceptions):
-        self._interceptions = interceptions
-
-   
-    def __str__(self):
-        return json.dumps(self.read())
+    @clean_sheets.setter
+    def clean_sheets(self, clean_sheets):
+        self._clean_sheets = clean_sheets
+        
 
 
+    """CRUD METHODS (And More convenient methods to help out in the future)"""
 
-
-    """CRUD METHODS (And More convenient methods to help out in the future) """  
-    
     def create(self):
         try:
-            # 
-            db.session.add(self)  
-            db.session.commit()  
+            db.session.add(self)
+            db.session.commit()
             return self
         except IntegrityError:
             db.session.remove()
             return None
 
-
     @staticmethod
     def getPlayerById(player_id):
-        return NFLPlayer.query.get(player_id)
+        return PremierLeaguePlayer.query.get(player_id)
 
     def update(self, player_id):
         try:
-            player_to_update = NFLPlayer.getPlayerById(player_id)
+            player_to_update = PremierLeaguePlayer.getPlayerById(player_id)
             if player_to_update:
                 player_to_update.name = self.name
                 player_to_update.team = self.team
@@ -224,18 +193,16 @@ class NFLPlayer(db.Model):
                 player_to_update.age = self.age
                 player_to_update.height = self.height
                 player_to_update.weight = self.weight
-                player_to_update.college = self.college
-                player_to_update.experience = self.experience
-                player_to_update.touchdowns = self.touchdowns
-                player_to_update.receptions = self.receptions
-                player_to_update.passing_yards = self.passing_yards
-                player_to_update.rushing_yards = self.rushing_yards
+                player_to_update.goals = self.goals
+                player_to_update.assists = self.assists
+                player_to_update.yellow_cards = self.yellow_cards
+                player_to_update.red_cards = self.red_cards
+                player_to_update.passes_completed = self.passes_completed
                 player_to_update.tackles = self.tackles
-                player_to_update.sacks = self.sacks
-                player_to_update.interceptions = self.interceptions
+                player_to_update.clean_sheets = self.clean_sheets
 
                 db.session.commit()
-                return NFLPlayer.getPlayerById(player_id)
+                return player_to_update
             else:
                 return None
         except IntegrityError:
@@ -243,61 +210,16 @@ class NFLPlayer(db.Model):
             return None
 
     def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-        return None
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return True
+        except:
+            db.session.remove()
+            return False
 
     @staticmethod
-    def getPlayerByName(player_name):
-        return NFLPlayer.query.filter_by(name=player_name).first()
+    def getAllPlayers():
+        return PremierLeaguePlayer.query.all()
 
-    def read(self):
-        return {
-            "name": self.name,
-            "team": self.team,
-            "position": self.position,
-            "jersey_number": self.jersey_number,
-            "age": self.age,
-            "height": self.height,
-            "weight": self.weight,
-            "college": self.college,
-            "experience": self.experience,
-            "touchdowns": self.touchdowns,
-            "receptions": self.receptions,
-            "passing_yards": self.passing_yards,
-            "rushing_yards": self.rushing_yards,
-            "tackles": self.tackles,
-            "sacks": self.sacks,
-            "interceptions": self.interceptions,
-            "id": self.id
-        }
-
-""" Test data creation - Database Creation and Testing """
-
-def initNFLPlayers():
-    print("Creating test data")
-    """Create database and tables"""
-    db.create_all()
     
-    """Tester data for table"""
-    
-    p1 = NFLPlayer(name="Tom Brady", team="Tampa Bay Buccaneers", position="Quarterback", jersey_number=12, age=44)
-    p2 = NFLPlayer(name="Patrick Mahomes", team="Kansas City Chiefs", position="Quarterback", jersey_number=15, age=25)
-    p3 = NFLPlayer(name="Aaron Rodgers", team="Green Bay Packers", position="Quarterback", jersey_number=12, age=37)
-    p4 = NFLPlayer(name="Derrick Henry", team="Tennessee Titans", position="Running Back", jersey_number=22, age=27)
-    p5 = NFLPlayer(name="Stefon Diggs", team="Buffalo Bills", position="Wide Receiver", jersey_number=14, age=27)
-    p6 = NFLPlayer(name="Travis Kelce", team="Kansas City Chiefs", position="Tight End", jersey_number=87, age=32)
-    p7 = NFLPlayer(name="Aaron Donald", team="Los Angeles Rams", position="Defensive Tackle", jersey_number=99, age=30)
-    p8 = NFLPlayer(name="Jalen Ramsey", team="Los Angeles Rams", position="Cornerback", jersey_number=20, age=26)
-    
-    nflplayersofficial = [p1, p2, p3, p4, p5, p6, p7, p8]
-    
-    """Builds sample player data"""
-    for player in nflplayersofficial:
-        try:
-            player.create()
-        except IntegrityError:
-            db.session.remove()
-            print(f"Records exist or error: {player.id}")
-
-  
