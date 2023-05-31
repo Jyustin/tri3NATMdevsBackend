4088de0cc08f7726ccbbce4088864ecc17d17dbc
BuddaBaker <prasithchilla@gmail.com>
2023-05-30 23:49:46 -0700

    failed change

--- model/trivia.py
+++ model/trivia.py
@@ -168,32 +168,34 @@ def get_questions():
 
 @app.route("/api/check_answer", methods=["POST"])
 def check_answer():
-    category = request.args.get('category')
-    points = request.args.get('points')
-    print(category,points)
-    print(request.get_json())
-    answer = request.get_json()["answer"]
+    data = request.get_json()
+    category = data["category"]
+    points = data["points"]
+    answer = data["answer"]
+
     for question in questions:
         if category == question["category"] and points == question["points"]:
             if answer == question["answer"]:
                 return jsonify({"result": "Correct"})
+
     return jsonify({"result": "Incorrect"})
 
-@app.route('/api/jeopardy')
-def data():
+@app.route('/api/jeopardy', methods=["GET"])
+def jeopardy():
     category = request.args.get('category')
     points = request.args.get('points')
-    print(category,points)
+
     for question in questions:
         if category == question["category"] and points == question["points"]:
             response = {
                 "question": question["question"]
             }
             return jsonify(response)
-    return '{ "Question": "Not found" }'
+
+    return jsonify({"question": "Not found"})
 
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run()
 
 # class Jeopardy(db.Model):
@@ -217,7 +219,7 @@ if __name__ == '__main__':
 # #print(Jeopardy.query.all())
 
 import sqlite3
-path = "instance/volumes/sqlite.db"
+path = "instancesqlite.db"
 connection = sqlite3.connect(path)
 def create_table():
     cursor = connection.cursor()
